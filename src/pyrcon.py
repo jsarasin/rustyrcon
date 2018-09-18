3a3a2fae071bde0b7c94822293366a94eb90f81c
James Sarasin <james.sarasin@gmail.com>
2018-09-18 02:02:39 -0600

    Added some chat functionality

--- src/pyrcon.py
+++ src/pyrcon.py
@@ -12,51 +12,61 @@ class ConnectionStatus:
     CLOSING = 2
     CLOSED = 3
 
-class PyRCON:
-    def __init__(self):
-        self.status = ConnectionStatus.CLOSED
-        self.address = None
-        self.port = None
-        self.password = None
+class PyRCON(WebSocketClient):
+    event_connected_cb = None
+    event_closed_cb = None
+    event_message_cb = None
 
-
-    def connect(self, address, port, pword):
-        self.address = address
-        self.port = port
-        self.password = pword
-
-
-
-class DummyClient(WebSocketClient):
     def opened(self):
-        # """
-        #   "Message": "{
-        #   'Message': 'hello',
-        #   'UserId': 0,
-        #   'Username': 'SERVER',
-        #   'Color': '#eee',
-        #   'Time': 1537168254
-        # }",
-        # """
-
-        self.send("{'Identifier':1, 'Message':'say hello','Name':'WebRcon'}")
-        # self.send("{'Identifier':1, 'Message':'chat.tail 10','Name':'WebRcon'}")
+        if self.event_connected_cb:
+            self.event_connected_cb()
+        self.send("{'Identifier':1, 'Message':'chat.tail','Name':'WebRcon'}")
         pass
 
     def closed(self, code, reason=None):
+        if self.event_closed_cb:
+            self.event_closed_cb()
+
         print("Closed down", code, reason)
 
     def received_message(self, m):
-        print(m)
+        if self.event_message_cb:
+            self.event_message_cb(m)
         sys.stdout.flush()
 
+#
+#
+# class DummyClient(WebSocketClient):
+#     def opened(self):
+#         self.send("{'Identifier':1, 'Message':'say hello','Name':'WebRcon'}")
+#         pass
+#
+#     def closed(self, code, reason=None):
+#         print("Closed down", code, reason)
+#
+#     def received_message(self, m):
+#         print(m)
+#         sys.stdout.flush()
+#
+#
+# if __name__ == '__main__':
+#     try:
+#         password = "1404817"
+#         ws = DummyClient('ws://108.61.239.97:28018/' + password, protocols=['http-only', 'chat'])
+#         ws.connect()
+#         ws.run_forever()
+#
+#     except KeyboardInterrupt:
+#         ws.close()
 
-if __name__ == '__main__':
-    try:
-        password = "1404817"
-        ws = DummyClient('ws://108.61.239.97:28018/' + password, protocols=['http-only', 'chat'])
-        ws.connect()
-        ws.run_forever()
-
-    except KeyboardInterrupt:
-        ws.close()
+# """
+#   "Message": "{
+#   'Message': 'hello',
+#   'UserId': 0,
+#   'Username': 'SERVER',
+#   'Color': '#eee',
+#   'Time': 1537168254
+# }",
+# """
+# self.send("{'Identifier':1, 'Message':'say hello','Name':'WebRcon'}")
+# # self.send("{'Identifier':1, 'Message':'chat.tail 10','Name':'WebRcon'}")
--- src/rustyrcon.py
+++ src/rustyrcon.py
@@ -4,8 +4,10 @@
 # ak
 # ammo.rifle
 
-
-import pyrcon
+import time
+import json
+import sys
+from pyrcon import PyRCON
 import gi
 
 gi.require_version('Gtk', '3.0')
@@ -13,14 +15,7 @@ from gi.repository import Gtk, GdkPixbuf, GObject, GLib, Gdk, cairo, Gio
 
 class MainWindow:
     def __init__(self):
-        self.traceroute_ping_interfaces = []
-        self.new_node = None
-
-
-        self.connect_builder_objects()
-
-class MainWindow:
-    def __init__(self):
+        self.pyrcon = None
         self.connect_builder_objects()
 
         self.window.show_all()
@@ -39,7 +34,7 @@ class MainWindow:
         builder.add_from_file("window.glade")
 
         self.window = builder.get_object("window")
-        self.window.connect("delete-event", Gtk.main_quit)
+        self.window.connect("delete-event", self.event_shutdown)
 
         # Connection Stage 1
         self.stack_connection_stage = builder.get_object("stack_connection_stage")
@@ -51,13 +46,18 @@ class MainWindow:
 
         # Connection Stage 2
         # Connection Stage 3
+        # Chat
         self.textentry_chat_who = builder.get_object("textentry_chat_who")
         self.buttoncolor_chat = builder.get_object("buttoncolor_chat")
         self.textentry_chat_message = builder.get_object("textentry_chat_message")
         self.button_chat_send = builder.get_object("button_chat_send")
+        self.button_chat_send.connect("clicked", self.send_chat_message)
+        # Console
         self.textentry_console = builder.get_object("textentry_console")
         self.button_console_send = builder.get_object("button_console_send")
         self.textview_chat = builder.get_object("textview_chat")
+        self.textbuffer_chat = Gtk.TextBuffer.new()
+        self.textview_chat.set_buffer(self.textbuffer_chat)
         self.treeview_players = builder.get_object("treeview_players")
         self.textentry_receiving_player = builder.get_object("textentry_receiving_player")
         self.treeview_loadout_item_list = builder.get_object("treeview_loadout_item_list")
@@ -67,6 +67,92 @@ class MainWindow:
 
     def event_button_clicked(self, button):
         self.stack_connection_stage.set_visible_child_name("page1")
+        self.pyrcon = PyRCON("ws://" + self.textentry_server_address.get_text() + ':' + self.textentry_server_port.get_text() + '/' + self.textentry_password.get_text(), protocols=['http-only', 'chat'])
+        self.pyrcon.event_connected_cb = self.event_connected
+        self.pyrcon.event_message_cb = self.event_rcon_message_received
+        self.pyrcon.connect()
+        # self.pyrcon.run()
+
+    def event_shutdown(self, widget, event):
+        if self.pyrcon is not None:
+            sys.stdout.flush()
+            self.pyrcon.close()
+            sys.stdout.flush()
+            self.pyrcon.close_connection()
+        Gtk.main_quit()
+
+        return False
+
+    def event_connected(self):
+        self.stack_connection_stage.set_visible_child_name("page2")
+
+    def event_rcon_message_received(self, message):
+
+        dicty = json.loads(message.data)
+        identifier = dicty['Identifier']
+        mtype = dicty['Type']
+        stacktrace = dicty['Stacktrace']
+        # assert(stacktrace == '')
+
+        # Chat Message
+        if(mtype == 'Chat'):
+            message = json.loads(dicty['Message'])
+            self.process_chat_message(message)
+        elif(mtype == "Log"):
+            pass
+        elif(mtype == "Generic"):
+            # Chat Message Received
+            if 'Message' in dicty:
+                print("mes", dicty['Messages'])
+                messages = json.loads(dicty['Message'])
+                for message in messages:
+                    GLib.idle_add(self.process_chat_message, message)
+        else:
+            print("Unhandled message type", dicty)
+
+        # print("Type:", dicty['message'])
+    def process_chat_message(self, message):
+        text = message['Message']
+        color = message['Color']
+        time = message['Time']
+        username = message['Username']
+        userid = message['UserId']
+
+        end_iter = self.textbuffer_chat.get_end_iter()
+        self.textbuffer_chat.insert(end_iter, username)
+        end_iter = self.textbuffer_chat.get_end_iter()
+        self.textbuffer_chat.insert(end_iter, ":")
+        end_iter = self.textbuffer_chat.get_end_iter()
+        self.textbuffer_chat.insert(end_iter, text)
+        end_iter = self.textbuffer_chat.get_end_iter()
+        self.textbuffer_chat.insert(end_iter, "\n")
+
+    def send_chat_message(self, button):
+        rcon_message = dict()
+        rcon_message['Message'] = self.textentry_chat_message.get_text()
+        rcon_message['UserId'] = 0
+        rcon_message['Username'] = self.textentry_chat_who.get_text()
+        rcon_message['Time'] = time.time()
+        rcon_message['Color'] = self.gtkcolor_to_web(self.buttoncolor_chat)
+
+        dicty = dict()
+        dicty['Identifier'] = 1
+        dicty['Name'] = "WebRcon"
+        dicty['Message'] = "say " + self.textentry_chat_message.get_text() #json.dumps(rcon_message)
+        print("Sending", json.dumps(dicty))
+        self.pyrcon.send(json.dumps(dicty))
+
+    def gtkcolor_to_web(self, widget):
+        color = self.buttoncolor_chat.get_color().to_floats()
+        string = "#" + '{0:x}'.format((int(255.0*(color[0]))))
+        string = string + '{0:x}'.format((int(255.0*(color[1]))))
+        string = string + '{0:x}'.format((int(255.0*(color[2]))))
+
+        print(string)
+        return ""
+
+
+
 
 mw = MainWindow()
 Gtk.main()
