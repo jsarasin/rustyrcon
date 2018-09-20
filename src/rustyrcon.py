45f2ece9d15b4cf60fc4b0b2511895aa7ce8d53a
James Sarasin <james.sarasin@gmail.com>
2018-09-19 22:51:31 -0600

    Chats 89% done perfectish. Console is function, added command completion which auto syncs with the server, cool!

--- src/rustyrcon.py
+++ src/rustyrcon.py
@@ -11,7 +11,7 @@ from pyrcon import PyRCON
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GdkPixbuf, GObject, GLib, Gdk, cairo, Gio, Pango
+from gi.repository import Gtk, GdkPixbuf, GObject, GLib, Gdk, cairo, Gio, Pango, GObject
 from betterbuffer import BetterBuffer
 
 class BufferManager:
@@ -22,14 +22,22 @@ class BufferManager:
 class MainWindow:
     def __init__(self):
         self.pyrcon = None
+        # Program initialization
+        self.have_command_and_var_cache = False
 
         # Chat message globals
         self.first_chat_message = True
+        self.last_chat_mark = None
 
+        # Console globals
+        self.first_console_message = True
+        self.last_console_entry_color = 0
+        self.last_console_mark = None
+
+
+        # GUI initialization
         self.connect_builder_objects()
-
         self.window.show_all()
-
         self.load_default_connection()
 
 
@@ -78,10 +86,24 @@ class MainWindow:
 
         # Console
         self.textentry_console = builder.get_object("textentry_console")
+        self.textentry_console.connect('activate', self.event_button_send_console)
         self.button_console_send = builder.get_object("button_console_send")
+        self.button_console_send.connect('clicked', self.event_button_send_console)
 
-        # Players
+        self.textview_console = builder.get_object("textview_console")
+        self.textbuffer_console = BetterBuffer()
+        self.textbuffer_console.create_tag("e1", paragraph_background="lightgrey", foreground='Black')
+        self.textbuffer_console.create_tag("e2", paragraph_background="darkgrey", foreground='White')
+        self.textview_console.set_buffer(self.textbuffer_console)
+
+
         self.treeview_players = builder.get_object("treeview_players")
+        self.entrycompletion_console = builder.get_object("entrycompletion_console")
+        self.entrycompletion_liststore = Gtk.ListStore(str,)# GObject.GType([str]) #TYPE_PYOBJECT
+        self.entrycompletion_console.set_model(self.entrycompletion_liststore)
+        self.entrycompletion_console.set_text_column(0)
+        # Players
+
 
         # Loadout Gift
         self.textentry_receiving_player = builder.get_object("textentry_receiving_player")
@@ -110,34 +132,112 @@ class MainWindow:
 
     def pyrcon_event_connected(self):
         GLib.idle_add(self.stack_connection_stage.set_visible_child_name, "page2")
+        self.send_console_message("global.find .")
 
     def pyrcon_event_rcon_message_received(self, message):
         dicty = json.loads(message.data)
         GLib.idle_add(self.safe_wee, dicty)
 
+    def process_chat_message(self, message):
+        pass
+    def process_console_message(self, message):
+        pass
+
+    def event_button_send_console(self, button):
+        self.send_console_message(self.textentry_console.get_text())
+        self.textentry_console.set_text('')
+
+    def send_console_message(self, message):
+        dicty = dict()
+        dicty['Identifier'] = 1
+        dicty['Name'] = "WebRcon"
+        dicty['Type'] = "Chat"
+        dicty['Message'] = message
+
+        self.pyrcon.send(json.dumps(dicty))
+        GLib.idle_add(self.textview_console.scroll_to_mark, self.last_console_mark, 0.1, True, 0.0, 0.5)
+
+    def process_initial_cache_message(self, message):
+        variables = []
+        commands = []
+        self.have_command_and_var_cache = True
+        # message = message.replace("\\n ", "\n")
+
+        command_section_start = message.find("Commands:\n") + 10
+
+        command_section = message[command_section_start:].split(sep="\n")
+        command_section = [item.strip() for item in command_section]
+
+        variables_section = message[11:command_section_start-12].split(sep="\n")
+        variables_section = [item.strip() for item in variables_section]
+
+        for variable in variables_section:
+            if variable == '':
+                continue
+            variable_name_end = variable.index(' ')
+            variable_name = variable[0:variable_name_end]
+
+            default_value_begin = variable.rindex('(')
+            default_value_end = variable.rindex(')')
+
+            description = variable[variable_name_end + 1:default_value_begin].strip()
+            default_value = variable[default_value_begin + 1:default_value_end]
+            variables.append((variable_name, description, default_value, ))
+
+        for command in command_section:
+            if command == '':
+                continue
+            command_name = command[0:command.index('(')]
+            description = command[command.index(')') + 1:].strip()
+            commands.append((command_name, description))
+
+        self.build_console_entry_completion(commands, variables)
+
+    def build_console_entry_completion(self, commands, variables):
+        for command in commands:
+            self.entrycompletion_liststore.append([command[0]])
+        for variable in variables:
+            self.entrycompletion_liststore.append([variable[0]])
+
+
+
     def safe_wee(self, dicty):
-        """
-         --- JSON Parse Error ---
-        message type: Generic
-         -- dicty --
-        {'Message': 'Saved 20,838 ents, cache(0.01), write(0.01), disk(0.00).', 'Identifier': 0, 'Type': 'Generic', 'Stacktrace': ''}
-         -- message --
-        Saved 20,838 ents, cache(0.01), write(0.01), disk(0.00).
-        Expecting value at 0
-
-         --- JSON Parse Error ---
-        message type: Generic
-         -- dicty --
-        {'Message': 'Saving complete', 'Identifier': 0, 'Type': 'Generic', 'Stacktrace': ''}
-         -- message --
-        Saving complete
-        Expecting value at 0
-
-        """
         identifier = dicty['Identifier']
         mtype = dicty['Type']
         stacktrace = dicty['Stacktrace']
         #assert(stacktrace == '')
+
+        if self.have_command_and_var_cache == False:
+            if 'Message' in dicty:
+                if dicty['Message'][0:10] == "Variables:":
+                    self.process_initial_cache_message(dicty['Message'])
+                    return
+
+        if dicty['Message'][0:1] != '{' and dicty['Message'] != '':
+            last_iter = self.textbuffer_console.get_end_iter()
+            begin_mark = self.textbuffer_console.create_mark(None, last_iter, True)
+            if not self.first_console_message:
+                last_iter = self.textbuffer_console.get_end_iter()
+                self.textbuffer_console.insert(last_iter, "\n")
+
+            self.first_console_message = False
+
+            last_iter = self.textbuffer_console.get_end_iter()
+            self.textbuffer_console.insert(last_iter, repr(dicty['Message'])[1:-1] + "")
+            last_iter = self.textbuffer_console.get_end_iter()
+            end_mark = self.textbuffer_console.create_mark(None, last_iter, True)
+            self.last_console_mark = end_mark
+
+            if self.last_console_entry_color == 0:
+                self.textbuffer_console.apply_tag_to_mark_range("e1", begin_mark, end_mark)
+                self.last_console_entry_color = 1
+            elif self.last_console_entry_color == 1:
+                self.textbuffer_console.apply_tag_to_mark_range("e2", begin_mark, end_mark)
+                self.last_console_entry_color = 0
+
+            self.textview_console.scroll_to_mark(self.last_console_mark, 0.1, True, 0.0, 0.5)
+
+
 
         try:
             # Chat Message
@@ -167,6 +267,7 @@ class MainWindow:
                 print("Unhandled message type", dicty)
                 sys.stdout.flush()
         except json.decoder.JSONDecodeError as e:
+            return
             print(" --- JSON Parse Error ---")
             print("message type:", mtype)
             print(" -- dicty --")
@@ -176,11 +277,11 @@ class MainWindow:
             print(e.msg, "at", e.pos)
             sys.stdout.flush()
 
-    def scroll_to_bottom(self):
+    def scroll_chat_to_bottom(self):
         # start, end = self.textbuffer_chat.get_bounds()
         #iter = self.textbuffer_chat.get_iter_at_line(self.textbuffer_chat.get_line_count() - 1)
         # self.textview_chat.scroll_to_iter(end, 0.0, False, 0, 1)
-        print(self.textview_chat.scroll_to_mark(self.last_mark, 0.1, True, 0.0, 0.5))
+        self.textview_chat.scroll_to_mark(self.last_chat_mark, 0.1, True, 0.0, 0.5)
 
 
     def process_chat_message(self, message):
@@ -212,7 +313,7 @@ class MainWindow:
         last_iter = self.textbuffer_chat.get_end_iter()
         end_mark = self.textbuffer_chat.create_mark(None, last_iter, True)
 
-        self.last_mark = end_mark
+        self.last_chat_mark = end_mark
 
         if username == "SERVER":
             self.textbuffer_chat.apply_tag_to_mark_range("server_chat", begin_chat_mark, end_mark)
@@ -221,7 +322,7 @@ class MainWindow:
             self.textbuffer_chat.apply_tag_to_mark_range("peasant_chat", begin_chat_mark, end_mark)
             self.textbuffer_chat.apply_tag_to_mark_range("peasant_uname", begin_chat_mark, end_uname_mark)
 
-        GLib.idle_add(self.scroll_to_bottom)
+        GLib.idle_add(self.scroll_chat_to_bottom)
 
     def unitymu_to_pangomu(self, text):
         original_text = text
@@ -243,7 +344,6 @@ class MainWindow:
                 break
 
             text = text[0:closing] + "\"" + text[closing:]
-            print("f")
             pos = closing + 1
         # Add a " after size tags
         pos = 0
@@ -262,7 +362,6 @@ class MainWindow:
                 break
 
             text = text[0:closing] + "\"" + text[closing:]
-            print("f")
             pos = closing + 1
 
         text = text.replace('<color=#', "<span fgcolor=\"#")
@@ -274,7 +373,6 @@ class MainWindow:
             print("Markup Error: ", original_text)
             return (False, original_text)
 
-        print("Result", text)
         return (True, text)
 
     def send_chat_message(self, button):
@@ -304,7 +402,7 @@ class MainWindow:
         dicty['Message'] = "say " + message_composed
 
         self.pyrcon.send(json.dumps(dicty))
-        GLib.idle_add(self.scroll_to_bottom)
+        GLib.idle_add(self.scroll_chat_to_bottom)
         self.textentry_chat_message.set_text('')
 
     def gtkcolor_to_web(self):
